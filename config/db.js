da77787ce36851b8a883d464c2c3398a51fa1cb4
unknown <kamesh>
2022-10-05 23:24:23 +0530

    added code

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose"
 
 
-export const Connection = async (username,password)=>{
+export const Connection = async (username, password) => {
     const URL = `mongodb+srv://kamesh:${password}@cluster0.ezdkdgq.mongodb.net/?retryWrites=true&w=majority`;
-    console.log(URL, 'thus is mongo url')
-    try{
-       await mongoose.connect(URL, {useUnifiedTopology:true, useNewUrlParser:true});
-       console.log("Database connected Secessfully")
-    } catch(err){
+    try {
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
+        console.log("Database connected Secessfully")
+    } catch (err) {
         console.log(`Error while connecting with the database`, err.message)
     }
 }
--- controllers/organization.js
+++ controllers/organization.js
@@ -3,41 +3,54 @@ import { validateRegistration } from "../validations/validateRegisterPayload.js"
 
 
 export const registerUser = async (req, res) => {
-    const requestBody = req.body;
-    const validatePayload = validateRegistration(requestBody);
+    try {
+        const requestBody = req.body;
+        const validatePayload = validateRegistration(requestBody);
 
-    if(validatePayload.error) {
-        res.status(401).send(validatePayload.error.message);
-    }
-    const regiterUser = await registerNewUser(requestBody);
+        if (validatePayload.error) {
+            res.status(401).send(validatePayload.error.message);
+        }
+        const regiterUser = await registerNewUser(requestBody);
 
-    if(regiterUser instanceof Error) {
-        return res.status(401).send(regiterUser.message);
+        if (regiterUser instanceof Error) {
+            return res.status(401).send(regiterUser.message);
+        }
+
+        res.status(200).send("Register Successfully");
     }
-    
-    res.status(200).send("Register Successfully");
+    catch (error) {
+        return res.status(500).send(error.message)
+    }
 }
 
 export const loginUser = async (req, res) => {
-    const requestBody = req.body;
-    const token = await loginOrgUser(requestBody);
+    try {
+        const requestBody = req.body;
+        const token = await loginOrgUser(requestBody);
 
-    if(token instanceof Error) {
-        return res.status(401).send(token.message);
+        if (token instanceof Error) {
+            return res.status(401).send(token.message);
+        }
+
+        res.status(200).json({ token });
+    } catch (error) {
+        return res.status(500).send(error.message)
     }
-
-    res.status(200).json({ token });
 }
 
 export const organizationData = async (req, res) => {
-    const userData = req.user;
-    const userName = userData?.userName;
+    try {
+        const userData = req.user;
+        const userName = userData?.userName;
 
-    const data = await getOrganizationData(userName);
+        const data = await getOrganizationData(userName);
 
-    if(data instanceof Error) {
-        return res.status(401).send(data.message);
+        if (data instanceof Error) {
+            return res.status(401).send(data.message);
+        }
+
+        res.status(200).json({ data });
+    } catch (error) {
+        return res.status(500).send(error.message)
     }
-
-    res.status(200).json({ data });
 }
